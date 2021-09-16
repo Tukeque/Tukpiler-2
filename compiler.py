f4ef938d2877149ec7772e334ceac21a1895b81e
Tukeque <TukequeMail@gmail.com>
2021-09-16 21:07:49 +0200

    Compiler.compile_func(): fill out with a bit of code (not tested yet)

--- compiler.py
+++ compiler.py
@@ -1,5 +1,5 @@
 from manager import Manager
-from var import Var
+from var import Var, Func
 from error import error
 from lexer import Reader
 import config
@@ -12,10 +12,12 @@ class Compiler:
         "none": 1,
         "array": -1
     }
-    vars: dict[str, Var] = {}
+    vars : dict[str, Var]  = {}
+    funcs: dict[str, Func] = {}
 
-    def __init__(self):
-        self.manager = Manager(self.vars)
+    def __init__(self, parser):
+        self.manager = Manager(self.vars, self.funcs, self.type_to_width)
+        self.parser = parser
 
     def get_var_names(self) -> list[str]:
         return [x for x in self.vars]
@@ -50,6 +52,66 @@ class Compiler:
         # todo miscs like in, out, pop?, push?, halt,
         #? ^ handle when calling functions.
 
+    def compile_func(self, expr: list[str]):
+        """
+        global funcrcl, func_name, func_ret_addr, vars
+        print(f"compiling function {tokens}")
+        #function add ( num x , num y ) - > num { ... }
+
+        name = tokens[1]
+        func_name = name
+        args = parse.split_list(tokens[tokens.index("(") + 1:tokens.index(")")], ",")
+        print(args)
+        return_type = tokens[tokens.index(")") + 3]
+
+        funcs[name] = Func(name, args, return_type)
+        funcrcl.append(f".function_{name}")
+        arg_table: dict[str, str] = {}
+
+        # return adress stack fix
+        return_address = get_reg()
+        funcrcl.append(f"POP {return_address}")
+        func_ret_addr = return_address
+
+        # extract arguments' pointers and create/overwrite the variables
+        before = copy(vars)
+        for arg in args:
+            reg = get_reg()
+            arg_table[arg[1]] = reg # setup an argument to reg table
+            funcrcl.append(f"POP {reg}")
+            vars[arg[1]] = Var(arg[1], arg[0], 1, argument = True, reg = reg)    
+
+        # compile insides
+        parse.parse(tokens[tokens.index("{") + 1:-1], True)
+
+        if funcrcl[-1][0:3] != "RET": # add return if it doesnt have one
+            funcrcl += ["PSH R0", f"PSH {func_ret_addr}", "RET"] # R0 = none
+
+        # free & clean up
+        for arg in arg_table: free_reg(arg_table[arg])
+        vars = before
+        free_reg(return_address)
+        """
+
+        name = expr[1]
+        args = Reader(expr[3:expr.index(")")])
+        ret_type = expr[expr.index(")") + 3]
+
+        func = Func(name, args, ret_type)
+        self.funcs[name] = func
+
+        self.manager.in_func = True
+        self.manager.emit(func.header())
+
+        # compile insides
+        self.parser.tokens = Reader(expr[expr.index("{") + 1:expr.index("}")])
+        self.compile(self.parser.parse())
+
+        # todo add return if doesn't have one (default case)
+
+        # free & clean up
+        self.manager.in_func = False
+
     def compile(self, exprs: Reader[list[str]]):
         def is_expr(expr: list[str]):
             if len(expr) >= 2:
@@ -65,8 +127,7 @@ class Compiler:
             if is_expr(expr):
                 self.compile_expr(expr)
             elif expr[0] == "function":
-                #self.compile_func(self, expr)
-                error("functions arent implemented yet")
+                self.compile_func(expr)
             elif expr[0] == "object":
                 error("objects arent implemented yet")
             elif expr[0] == "if":
