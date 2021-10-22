bb16097809e5c1fc8f86413ced983f71249533bd
Tukeque <TukequeMail@gmail.com>
2021-10-22 23:20:44 +0200

    error.py: make errors more clear and add debugging messages

--- error.py
+++ error.py
@@ -1,11 +1,12 @@
 import config
+from rich import print
 
 errors = []
 
 def error(msg: str):
-    print(f"<!!error!!> -> {msg}")
+    print(f"[red]<!!error!!> -> {msg}")
     errors.append(msg)
 
 def debug(msg):
     if config.debug:
-        print(f"[debug] {msg}")
+        print(f"[yellow]\[debug] {msg}")
